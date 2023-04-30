6b13028a8aac47280cae591b25597f797f4db0bf
srquike <srquike@outlook.com>
2023-04-30 11:13:15 -0600

    Agregar clase para la entidad Movie

---
+++ CursoAngular.BOL/Entities/MovieEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace CursoAngular.BOL;
+public class MovieEntity
+{
+    public int MovieId { get; set; }
+    public string? Title { get; set; }
+    public DateOnly? ReleaseDate { get; set; }
+    public string? PosterUrl { get; set; }
+    public string? TrailerUrl { get; set; }
+    public string? MpaaRating { get; set; }
+
+    public virtual ICollection<GenreEntity> Genres { get; set; } = new List<GenreEntity>();
+    public virtual ICollection<StarEntity> Cast { get; set; } = new List<StarEntity>();
+    public virtual ICollection<CinemaEntity> Cinemas { get; set; } = new List<CinemaEntity>();
+}
