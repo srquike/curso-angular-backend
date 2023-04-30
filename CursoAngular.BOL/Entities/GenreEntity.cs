46a4807410c83e8e0884f159c118722eed29c488
srquike <srquike@outlook.com>
2023-04-30 11:13:05 -0600

    Agregar clase para la entidad Genre

---
+++ CursoAngular.BOL/Entities/GenreEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace CursoAngular.BOL;
+public class GenreEntity
+{
+    public int GenreId { get; set; }
+    public string? Name { get; set; }
+
+    public virtual ICollection<MovieEntity> Movies { get; set; } = new List<MovieEntity>();
+}
