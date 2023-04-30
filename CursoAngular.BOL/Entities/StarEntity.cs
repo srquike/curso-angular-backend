e4b48b59d92e600553ac1c1f62e5dacfb60133c1
srquike <srquike@outlook.com>
2023-04-30 11:13:25 -0600

    Agregar clase para la entidad Star

---
+++ CursoAngular.BOL/Entities/StarEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CursoAngular.BOL;
+public class StarEntity
+{
+    public int ActorId { get; set; }
+    public string? Name { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
+    public string? Character { get; set; }
+    public string? PhotographyURL { get; set; }
+
+    public virtual ICollection<MovieEntity> Movies { get; set; } = new List<MovieEntity>();
+}
