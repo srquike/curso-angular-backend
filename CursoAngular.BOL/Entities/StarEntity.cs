384750e3976398e6d09a15d4ce9dc1a9d94a4e24
srquike <srquike@outlook.com>
2023-04-30 12:20:15 -0600

    Eliminar propiedad Character

--- CursoAngular.BOL/Entities/StarEntity.cs
+++ CursoAngular.BOL/Entities/StarEntity.cs
@@ -4,7 +4,6 @@ public class StarEntity
     public int ActorId { get; set; }
     public string? Name { get; set; }
     public DateOnly? DateOfBirth { get; set; }
-    public string? Character { get; set; }
     public string? PhotographyURL { get; set; }
 
     public virtual ICollection<MovieEntity> Movies { get; set; } = new List<MovieEntity>();
