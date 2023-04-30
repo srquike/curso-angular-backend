4bc5c029c446c5acbefeb2c1ae6136dd42bf5c01
srquike <srquike@outlook.com>
2023-04-30 13:04:54 -0600

    Modificar tipo de dato de la propiedad ReleaseDate de DateOnly? a DateTime?

--- CursoAngular.BOL/Entities/MovieEntity.cs
+++ CursoAngular.BOL/Entities/MovieEntity.cs
@@ -3,7 +3,7 @@ public class MovieEntity
 {
     public int MovieId { get; set; }
     public string? Title { get; set; }
-    public DateOnly? ReleaseDate { get; set; }
+    public DateTime? ReleaseDate { get; set; }
     public string? PosterUrl { get; set; }
     public string? TrailerUrl { get; set; }
     public string? MpaaRating { get; set; }
