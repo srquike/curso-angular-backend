1f6c859eb2eef351c8027f96716c7b793e56b00c
srquike <srquike@outlook.com>
2023-04-30 11:12:46 -0600

    Agregar clase para la entidad Cinema

---
+++ CursoAngular.BOL/Entities/CinemaEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoAngular.BOL;
+public class CinemaEntity
+{
+    public int CinemaId { get; set; }
+    public string? Name { get; set; }
+    public string? Location { get; set; }
+
+    public virtual ICollection<MovieEntity> Movies { get; set; } = new List<MovieEntity>();
+}
