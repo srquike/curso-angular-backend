13b5c5d726ac6264ba665ce183111ef99531ceac
srquike <srquike@outlook.com>
2023-05-04 13:42:35 -0600

    Agregar extension de la clase HttpContext para agregar Headers personalizados

---
+++ CursoAngular.API/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoAngular.API.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static void SetPaginationParameters(this HttpContext httpContext, int itemsCount)
+        {
+            httpContext?.Response.Headers.Add("itemsCount", itemsCount.ToString());
+        }
+    }
+}
