5ddc246abdd4f6aedc28ef74ef98b6c8b975a199
srquike <srquike@outlook.com>
2023-05-04 13:42:54 -0600

    Agregar clase encargada de la paginacion

---
+++ CursoAngular.API/Tools/Pagination.cs
@@ -0,0 +1,16 @@
+﻿namespace CursoAngular.API.Tools
+{
+    public class Pagination
+    {
+        private int itemsToDisplay = 10;
+        private int MaxItemsToDisplay { get; } = 50;
+
+        public int PageNumber { get; set; } = 1;
+        public int ItemsToDisplay { get => itemsToDisplay; set => itemsToDisplay = (value > MaxItemsToDisplay) ? MaxItemsToDisplay : value; }
+
+        public int GetSkipCount()
+        {
+            return (PageNumber - 1) * ItemsToDisplay;
+        }
+    }
+}
