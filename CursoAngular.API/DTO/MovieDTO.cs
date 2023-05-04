6ffde4ea51deef6a8b31ee2dea5715d1aa7f4098
srquike <srquike@outlook.com>
2023-05-04 13:42:02 -0600

    Agregar clase DTO para Movies

---
+++ CursoAngular.API/DTO/MovieDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace CursoAngular.API.DTO
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "El {0} de la movie es requerido")]
+        [StringLength(maximumLength: 10, ErrorMessage = "El {0} de la movie debe tener máximo 10 caracteres")]
+        public string? Title { get; set; }
+        public string? ReleaseDate { get; set; }
+        public string? PosterUrl { get; set; }
+        public string? TrailerUrl { get; set; }
+        public string? MpaaRating { get; set; }
+    }
+}
