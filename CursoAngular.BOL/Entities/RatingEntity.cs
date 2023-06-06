dd71273032fec8ed90fa25289a41b726b4349197
srquike <srquike@outlook.com>
2023-06-06 12:09:46 -0600

    Agregar clase para la tabla Ratings

---
+++ CursoAngular.BOL/Entities/RatingEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CursoAngular.BOL.Entities
+{
+    public class RatingEntity : BaseEntity
+    {
+        public int Scoring { get; set; }
+        public int MovieId { get; set; }
+        public string? UserId { get; set; }
+
+        public virtual MovieEntity? Movie { get; set; }
+        public virtual IdentityUser? User { get; set; }
+    }
+}
