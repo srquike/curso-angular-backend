43cad7a15571cfa459e58432cda8f79ad8ad9d13
srquike <srquike@outlook.com>
2023-09-24 14:29:27 -0600

    Agregar migraciones

--- CursoAngular.DAL/Contexts/CursoAngularDbContext.cs
+++ CursoAngular.DAL/Contexts/CursoAngularDbContext.cs
@@ -23,43 +23,16 @@ public class CursoAngularDbContext : IdentityDbContext
 
         modelBuilder.Entity<CinemaEntity>(entity =>
         {
-            entity.ToTable("Cinemas");
-
-            entity.HasKey(e => e.Id);
-
-            entity.Property(e => e.Name)
-                .HasColumnType("varchar(50)");
             entity.Property(e => e.Location)
                 .HasColumnType("geography");
 
             entity.HasMany(d => d.MovieCinemas).WithOne(p => p.Cinema)
                 .HasForeignKey(p => p.CinemaId)
                 .HasConstraintName("FK_Cinemas_MoviesCinemas_CinemaId");
-
-            //entity.HasMany(c => c.Movies)
-            //    .WithMany(m => m.Cinemas)
-            //    .UsingEntity<MovieCinemaEntity>(
-            //        "MoviesCinemas",
-            //        l => l.HasOne<MovieEntity>().WithMany().HasForeignKey(e => e.MovieId),
-            //        r => r.HasOne<CinemaEntity>().WithMany().HasForeignKey(e => e.CinemaId));
         });
 
         modelBuilder.Entity<MovieEntity>(entity =>
         {
-            entity.ToTable("Movies");
-
-            entity.HasKey(e => e.Id);
-
-            entity.Property(e => e.Title)
-                .HasColumnType("varchar(100)");
-            entity.Property(e => e.ReleaseDate);
-            entity.Property(e => e.PosterUrl)
-                .HasColumnType("varchar(max)");
-            entity.Property(e => e.TrailerUrl)
-                .HasColumnType("varchar(max)");
-            entity.Property(e => e.MpaaRating)
-                .HasColumnType("varchar(50)");
-
             entity.HasMany(p => p.Casting).WithOne(d => d.Movie)
                 .HasForeignKey(d => d.MovieId)
                 .HasConstraintName("FK_Movies_StarsMovies_MovieId");
@@ -75,13 +48,6 @@ public class CursoAngularDbContext : IdentityDbContext
 
         modelBuilder.Entity<GenreEntity>(entity =>
         {
-            entity.ToTable("Genres");
-
-            entity.HasKey(e => e.Id);
-
-            entity.Property(e => e.Name)
-                .HasColumnType("varchar(50)");
-
             entity.HasMany(p => p.GenreMovies).WithOne(d => d.Genre)
                 .HasForeignKey(d => d.GenreId)
                 .HasConstraintName("FK_Genres_GenresMovies_GenreId");
@@ -89,16 +55,6 @@ public class CursoAngularDbContext : IdentityDbContext
 
         modelBuilder.Entity<StarEntity>(entity =>
         {
-            entity.ToTable("Stars");
-
-            entity.HasKey(e => e.Id);
-
-            entity.Property(e => e.Name)
-                .HasColumnType("varchar(50)");
-            entity.Property(e => e.DateOfBirth);
-            entity.Property(e => e.PhotographyURL)
-                .HasColumnType("varchar(max)");
-
             entity.HasMany(p => p.StarMovies).WithOne(d => d.Star)
                 .HasForeignKey(d => d.StarId)
                 .HasConstraintName("FK_Stars_StarsMovies_StarId");
